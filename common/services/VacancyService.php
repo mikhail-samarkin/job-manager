6e15b26a372f6c9c29cac98bce4cc23bddbe2a70
Mikhal Samarkin <mikhail.samarkin@simbirsoft.com>
2018-12-01 15:07:57 +0400

    update VacancyService

--- common/services/VacancyService.php
+++ common/services/VacancyService.php
@@ -1,13 +1,41 @@
 <?php
 namespace app\common\services;
 
-use app\common\models\entity\VacancyRecord;
+use app\common\dispatchers\EventDispatcher;
+use app\common\entities\Vacancy\Vacancy;
+use app\common\entities\Vacancy\VacancyId;
+use app\common\repositories\VacancyRepository;
 
 class VacancyService
 {
+    private $vacancyRepository;
+    private $eventDispatcher;
+
+    public function __construct(VacancyRepository $vacancyRepository, EventDispatcher $eventDispatcher)
+    {
+        $this->vacancyRepository = $vacancyRepository;
+        $this->eventDispatcher = $eventDispatcher;
+    }
+
     public function getPreparedVacancies()
     {
-        $vacancies = VacancyRecord::find()->asArray()->all();
+        $vacancies = $this->vacancyRepository->all();
         return $vacancies;
+    }
+
+    public function generateRandomVacancies()
+    {
+        $count = rand(10, 100);
+        $faker = \Faker\Factory::create();
+
+        for($i = 0; $i < $count; $i++) {
+            $title = $faker->title;
+            $description = $faker->text;
+            $vacancy = new Vacancy($this->vacancyRepository->nextId(), $title, $description);
+            $this->vacancyRepository->add($vacancy);
+            $this->eventDispatcher->dispatch($vacancy->releaseEvents());
+        }
+
+        return $count;
     }
 }
