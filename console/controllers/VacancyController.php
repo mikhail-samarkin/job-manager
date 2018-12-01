8e7f9d1eb3d47dabc8d064832c75aa6c54b9180f
Mikhal Samarkin <mikhail.samarkin@simbirsoft.com>
2018-12-01 15:07:11 +0400

    add console command: php yii vacancy/generated

---
+++ console/controllers/VacancyController.php
@@ -0,0 +1,25 @@
+<?php
+namespace app\console\controllers;
+
+use app\common\services\VacancyService;
+use yii\base\Module;
+use yii\helpers\Console;
+
+class VacancyController extends \yii\console\Controller
+{
+    private $vacancyService;
+
+    public function __construct(string $id, Module $module, array $config = [], VacancyService $vacancyService)
+    {
+        $this->vacancyService = $vacancyService;
+        parent::__construct($id, $module, $config);
+    }
+
+    /**
+     * Generate random vacancies
+     */
+    public function actionGenerate() {
+        $count = $this->vacancyService->generateRandomVacancies();
+        Console::output('Generated '.$count.' vacancies');
+    }
+}
